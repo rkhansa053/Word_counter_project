21f9b6006ae095c7cdae76adf1ac0cd7cab97f40
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-06-23 21:35:16 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+
+// Import the inquirer module, which is a command line interface for node.js
+import inquirer from "inquirer"
+
+const answers: {
+    Sentence: string
+} = await inquirer .prompt([
+    {
+        name: "senntence",
+        type: "input",
+        message: "Enter your sentence to count the words:  "
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+// print the array of words to the console
+console.log(words);
+
+// print the words count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
+
+//  trim => to remove spaces between words
+
+
